b567f00487b41379b48f05c17393039fc442e7ba
Agustina Bedrossian <agusbedro@gmail.com>
2021-09-16 22:26:33 -0700

    redimension de imagenes funcionando

--- js/main.js
+++ js/main.js
@@ -1,41 +1,26 @@
 let canvas = document.getElementById("myCanvas");
 let ctx = canvas.getContext("2d");
-let canvasWidht = canvas.width;
-let canvasHeight = canvas.height;
 let pencil = false;
-let lines = [];
-let lastClickedFigure = null;
-let isMouseDown = false;
 let eraser = false;
-let activarPintado = false;
-
-function setPencil(){
-    pencil = true;
-    eraser = false;
-}
-
+let paint = false;
 
 function onMouseDown(e){
     if(pencil || eraser){
-        activarPintado = true;
+        paint = true;
         onMouseMove(e);
     }
 }
 
 function onMouseMove(e){
-    if(activarPintado){
-        isMouseDown = true;
+    if(paint){
         let color;
-    
-        if(pencil){
-            color = document.getElementById("paleta").value;
-            ctx.lineWidth = 25;
-        }
-        else if(eraser){
-            color = 'white';
-            ctx.lineWidth = 25;
-        }
 
+        if(pencil)
+            color = document.getElementById("paleta").value;
+        else if(eraser)
+            color = 'white';
+        
+        ctx.lineWidth = 25;
         ctx.strokeStyle = color;
         ctx.fill();    
         ctx.lineCap = "round";
@@ -47,9 +32,13 @@ function onMouseMove(e){
 }
 
 function onMouseUp(){
-    isMouseDown = false;
-    activarPintado = false;
+    paint = false;
     ctx.beginPath();
+}
+
+function draw(){
+    pencil = true;
+    eraser = false;
 }
 
 function erase(){
@@ -58,13 +47,9 @@ function erase(){
 }
 
 canvas.addEventListener('mousedown',onMouseDown, false);
-
 canvas.addEventListener('mousemove', onMouseMove, false);
-
 canvas.addEventListener('mouseup', onMouseUp, false);
-
-document.getElementById("pencil").addEventListener('click', setPencil, false);
-
+document.getElementById("pencil").addEventListener('click', draw, false);
 document.getElementById("eraser").addEventListener('click', erase, false);
 
 
--- js/uploadImage.js
+++ js/uploadImage.js
@@ -1,50 +1,48 @@
 function addImage(){
     var img = new Image();
     img.src = x;
-
     img.onload = function(){
         ctx.drawImage(img, 0, 0);
     }
 }
 
-let image = document.getElementById("image");
-
-image.addEventListener('change', codigo, false);
-
-
-function codigo(e){
+function uploadImage(e){
     if (e.target.files) {
-        let img = e.target.files[0];
+        let file = e.target.files[0];
         let reader = new FileReader();
-        reader.readAsDataURL(img);
+        reader.readAsDataURL(file);
         reader.onload = function (e) {
             let image = new Image();
             image.src = e.target.result;
+            
             image.onload = function (ev) {
-                resize(image.width, image.height);
-                ctx.drawImage(image, 0, 0,700,400);
+                
+                let size;
+                let width = image.width;
+                let height = image.height;
+
+                if(width < height){ 
+                    size = canvas.height / height;
+                    width = width * size;
+                    height = height * size;
+                } 
+                else if (width > height){
+                    size = canvas.width / width;
+                    width = width * size;
+                    height = height * size;
+                } 
+                else {
+                    let sizeW = canvas.width / width;
+                    let sizeH = canvas.height  / height;
+                    width = width * sizeW;
+                    height = height * sizeH;
+                }
+
+                ctx.drawImage(image, 0, 0, width, height);
             }
-        }
-        
+        } 
     }
 }
 
-function resize(imgW, imgH){
-    if(imgW < imgH){ 
-        let porc = canvasH / imgH;
-        imgW = imgW * porc;
-        imgH = imgH * porc;
-    } 
-    else if (imgW > imgH){
-        let porc = canvasW / imgW;
-        imgW = imgW * porc;
-        imgH = imgH * porc;
-    } 
-    else {
-        let porcW = canvasW / imgW;
-        let porcH = canvasH  / imgH;
-        imgW = imgW * porcW;
-        imgH = imgH * porcH;
-    }
-}
-
+let image = document.getElementById("image");
+image.addEventListener('change', uploadImage, false);
